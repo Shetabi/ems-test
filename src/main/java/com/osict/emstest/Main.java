c96fce8fab3aa2b8131c0de18e3cd4726d84bc79
shetabivash <mohammad.shetabivash@gmail.com>
2015-07-28 11:29:43 +0430

    First commit

---
+++ src/main/java/com/osict/emstest/Main.java
@@ -0,0 +1,13 @@
+package com.osict.emstest;
+
+/**
+ *
+ * @author Mahdi-Shivaeifar <Shivaeifar.m@Gmail.com>
+ * Created on Jul 28, 2015, 11:04:08 AM
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
